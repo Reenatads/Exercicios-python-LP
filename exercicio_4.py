564c301128afe54a0279fed858584ad0612321ab
Reenatads <49541299+Reenatads@users.noreply.github.com>
2021-11-07 20:01:21 -0300

    Update exercicio_4.py

--- exercicio_4.py
+++ exercicio_4.py
@@ -8,15 +8,30 @@
 print("4) Implementar uma função que retorne verdadeiro se o número for primo (falso caso contrário). Testar de 1 a 100.")
 print("")
 
-var_final = 100
-num_divisoes = 0
+var_entrada = int(input("Digite um número: "))                                  # Capturando valor de entrada
+primos = []
+ 
 
-for i in range(var_inicio, var_final + 1):
-    if var_inicio % i == 0 :
-        print("O número " + str(var_inicio) + " é primo.")
-    else:   
-        if var_inicio % 2 != 0:
-            print("O número " + str(var_inicio) + " é primo.")
+for i in range(1, var_entrada + 1):                                             
+    
+    if var_entrada % i == 0 :
+        primos.append(i) 
+    else:
+        pass   
+        
+        
+            
+if len(primos) == 2 :
+                print("O número " + str(var_entrada) + " é primo.")
+else:
+                print("O número " + str(var_entrada) + " não é primo") 
+    
+         
+
+
+        
+
+            
 
     
 
